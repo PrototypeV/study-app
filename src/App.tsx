98df5bb13930b305d2333c17ffe213fe4faf3a01
AverageCatOwner <byte.vee2@gmail.com>
2024-12-30 18:29:43 -0500

    Organized pages and components into folders. Added functionality to the navbar buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import Navbar from './Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Calender from './pages/Calender'
+import Flashcards from './pages/Flashcards'
+import Focus from './pages/Focus'
+import Notes from './pages/Notes'
+import Tts from './pages/Tts'
 import './App.css'
 
 function App() {
-  return <div><Navbar/></div>
+  return <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Calender" element={<Calender />} />
+        <Route path="/Flashcards" element={<Flashcards />} />
+        <Route path="/Focus" element={<Focus />} />
+        <Route path="/Notes" element={<Notes />} />
+        <Route path="/Tts" element={<Tts />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 }
 
 export default App
