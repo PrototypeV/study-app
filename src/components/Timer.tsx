28bb3b25b48bb238304aaa30e21a95973637e630
PrototypeV <byte.vee2@gmail.com>
2025-01-06 10:05:22 -0500

    time remaining dynamically gets session time

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,17 +3,17 @@ import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 
 function Timer() {
-    const [session, setSession] = useState(0);
+    const [session, setSession] = useState();
 
-    function sessionTime(){
-        setSession(1);
+    function sessionTime(value: any){
+        setSession(value);
     }
 
   return (
     <>
         <p>Time Remaining: {session}</p>
         <p>Session Time:</p>
-        <Form.Select aria-label="Default select example">
+        <Form.Select aria-label="Default select example" onChange={e=>sessionTime(e.target.value)}>
             <option value="1">10 mins</option>
             <option value="2">20 mins</option>
             <option value="3">30 mins</option>
@@ -24,7 +24,7 @@ function Timer() {
             <option value="2">10 mins</option>
             <option value="3">15 mins</option>
         </Form.Select>
-        <Button variant="success" onClick={sessionTime}>Start</Button>
+        <Button variant="success">Start</Button>
         <Button variant="danger">Stop</Button>
         <Button variant="secondary">Cancel</Button>
     </>
